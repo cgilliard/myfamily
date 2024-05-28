265b6ac987b48b732cf97318468807da88bd674b
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-05-27 19:09:33 -0700

    push updated docs

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -2,5 +2,12 @@
 
 echo "Building docs";
 make doc
-rm -rf docs/html/*
-cp -rp doc/html/* docs/html
+git pull
+git clone https://anything:$(GithubPat)@github.com/cgilliard/myfamily.git myfamilydocs
+cd myfamilydocs
+rm -rf docs/html
+cp -rp ../doc/html/* docs/html
+git config user.name "Pipelines-Bot"
+git config --global user.email "pipelinesbot.noreply@example.com"
+git config --global user.name "Pipelines-Bot"
+git push https://$(GithubPat)@github.com/cgilliard/myfamily.git
