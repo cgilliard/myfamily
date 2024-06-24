a5f50d29d167220677d779d24c5d19f2f131c3a9
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-06-24 08:01:04 -0700

    include traits

--- base/traits.h
+++ base/traits.h
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <base/traits_base.h>
+
 #define TRAIT_APPEND(T)                                                        \
 	TRAIT_REQUIRED(T, Result, append, T##Ptr *dst, T##Ptr *src)
 #define TRAIT_DEEP_COPY(T)                                                     \
