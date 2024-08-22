66bfd114011a6358f8a0139e71dc1837cd9f513f
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-08-21 18:12:31 -0700

    create main doxygen page

---
+++ doxygen.c
@@ -0,0 +1,11 @@
+/**
+ * @mainpage
+ *
+ * Welcome to the My Family Library Documentation.
+ *
+ * This is the main page of the documentation for the Library.
+ *
+ * For detailed information, refer to:
+ * - \ref core "Core Library Documentation"
+ * - \ref main "Main Documentation"
+ */
