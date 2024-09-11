96f07476fec437f16948962a74275bbf706b707d
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-09-10 21:40:44 -0700

    update configure for minimal builds

--- scripts/configure_homebrew.sh
+++ scripts/configure_homebrew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure \
+./configure --minimal=false \
 	--with-criterion-include=${HOME}/homebrew/Cellar/criterion/2.4.2/include \
 	--with-criterion-lib=${HOME}/homebrew/Cellar/criterion/2.4.2/lib
 
