b2be341aca1735df1f02cdd4ae23c9ce658a69cf
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-10-24 11:34:10 -0700

    save

--- base/os.h
+++ base/os.h
@@ -13,3 +13,35 @@
 // limitations under the License.
 
 #include <base/os_impl.h>
+#include <base/types.h>
+/*
+// Memory Management
+void *mymalloc(u64 size, bool zeroed);
+void myfree(void *ptr);
+void *myrealloc(void *ptr, u64 size);
+
+typedef enum Whence {
+	WhenceStart,
+	WhenceCur,
+} Whence;
+
+typedef struct DirectoryEntry {
+	const char *name;
+} DirectoryEntry;
+
+typedef struct Directory {
+	const u64 count;
+	DirectoryEntry *entries;
+} Directory;
+
+// File System
+i64 create_file(const u8 *path);
+i64 create_directory(const u8 *path);
+i64 remove(const u8 *path);
+i64 open(const u8 *path, u64 flags);
+i64 close(i64 handle);
+i64 seek(i64 handle, i64 offset, Whence whence);
+i64
+
+	// Process Management
+*/
