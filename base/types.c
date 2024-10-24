fdddac4ee8a2c95656df85f25153b7aae7fc3777
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-10-24 11:57:54 -0700

    remove unneeded files

--- base/types.c
+++ base/types.c
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <base/print_util.h>
 #include <base/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 bool __is_little_endian() {
 	u16 test = 0x1;
@@ -22,26 +23,33 @@ bool __is_little_endian() {
 
 void __attribute__((constructor)) __check_64bit_arch__() {
 #if !defined(__x86_64__) && !defined(_M_X64) && !defined(__aarch64__)
-	panic("Error: This program requires a 64-bit architecture to run.");
+	printf("Error: This program requires a 64-bit architecture to run.\n");
+	exit(-1);
 #endif // arch
 
-	if (__SIZEOF_SIZE_T__ != 8)
-		panic("Error: This program requires a 64-bit architecture to run.");
+	if (__SIZEOF_SIZE_T__ != 8) {
+		printf("Error: This program requires a 64-bit architecture to run.");
+		exit(-1);
+	}
 	if (sizeof(u8) != 1 || sizeof(u16) != 2 || sizeof(u32) != 4 || sizeof(u64) != 8 ||
 		sizeof(u128) != 16 || sizeof(i8) != 1 || sizeof(i16) != 2 || sizeof(i32) != 4 ||
 		sizeof(i64) != 8 || sizeof(i128) != 16 || sizeof(f32) != 4 || sizeof(f64) != 8 ||
 		sizeof(bool) != 1) {
-		panic("Invalid data type size! Check your c compiler configuration options.");
+		printf("Invalid data type size! Check your c compiler configuration options.\n");
+		exit(-1);
 	}
 
 	if (!__is_little_endian()) {
-		panic("Big endian systems not supported");
+		printf("Big endian systems not supported");
+		exit(-1);
 	}
 
 	if (sizeof(au64) != 8) {
-		panic("Error: Unexpected size for atomic_ullong. Check your compiler and architecture.");
+		printf("Error: Unexpected size for atomic_ullong. Check your compiler and architecture.\n");
+		exit(-1);
 	}
 	if (sizeof(au32) != 4) {
-		panic("Error: Unexpected size for atomic_uint. Check your compiler and architecture.");
+		printf("Error: Unexpected size for atomic_uint. Check your compiler and architecture.\n");
+		exit(-1);
 	}
 }
