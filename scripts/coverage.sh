7d54bf5e713dc31034c111d50d6055681982e427
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-11-11 23:14:59 -0800

    check for files that have no lines

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -43,19 +43,22 @@ echo "$BLUE--------------------------------"\
 "--------------------$RESET";
    cd $dir;
    for gcno in *.gcno; do
-      percent=`gcov $gcno | grep "^Lines" | head -1 | cut -f2 -d ' ' | cut -f2 -d ':' | cut -f1 -d '%' | tr -d \\n`;
-      if [ "$percent" = "" ]; then
-         percent=0.00;
-      fi;
-      lines=`gcov $gcno | grep "^Lines" | head -1 | cut -f4 -d ' ' | tr -d \\n`;
-      if [ "$lines" = "" ]; then
-         lines="0";
+      if [ -f ${gcno%.gcno}.gcda ] ; then
+         percent=`gcov $gcno | grep "^Lines" | head -1 | cut -f2 -d ' ' | cut -f2 -d ':' | cut -f1 -d '%' | tr -d \\n`;
+         if [ "$percent" = "" ]; then
+            percent=0.00;
+         fi;
+         lines=`gcov $gcno | grep "^Lines" | head -1 | cut -f4 -d ' ' | tr -d \\n`;
+         if [ "$lines" = "" ]; then
+            lines="0";
+         fi
+
+         ratio=`awk "BEGIN {print $percent / 100}"`;
+         covered=`awk "BEGIN {print int($ratio * $lines)}"`;
+         linessum=`awk "BEGIN {print $linessum + $lines}"`;
+         coveredsum=`awk "BEGIN {print $coveredsum + $covered}"`;
+         printf "$GREEN%-20s$RESET $YELLOW%-10s$RESET $CYAN%s$RESET\n" "${gcno%.gcno}.c" "$percent%" "$lines";
       fi
-      ratio=`awk "BEGIN {print $percent / 100}"`;
-      covered=`awk "BEGIN {print int($ratio * $lines)}"`;
-      linessum=`awk "BEGIN {print $linessum + $lines}"`;
-      coveredsum=`awk "BEGIN {print $coveredsum + $covered}"`;
-      printf "$GREEN%-20s$RESET $YELLOW%-10s$RESET $CYAN%s$RESET\n" "${gcno%.gcno}.c" "$percent%" "$lines";
    done
    cd ..;
 done
