887c595bb679abd8502a8c14f7ca96521e39c523
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-11-11 20:27:50 -0800

    formatting

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,19 +2,36 @@
 
 . ./scripts/env.sh
 
+if [ "$NO_COLOR" = "" ]; then
+   GREEN="\x1b[32m";
+   CYAN="\x1b[36m";
+   YELLOW="\x1b[33m";
+   BRIGHT_RED="\x1b[91m";
+   RESET="\x1b[0m";
+   BLUE="\x1b[34m";
+else
+   GREEN="";
+   CYAN="";
+   YELLOW="";
+   BRIGHT_RED="";
+   RESET="";
+   BLUE="";
+fi
+
+
 linessum=0;
 coveredsum=0;
 
 for dir in $subdirs; do
-echo "--------------------------------"\
+echo "$BLUE--------------------------------"\
 "--------------------------------"\
 "--------------------------------"\
-"--------------------";
+"--------------------$RESET";
 printf "%-20s %-10s %s\n" "$dir" "percent" "lines";
-echo "--------------------------------"\
+echo "$BLUE--------------------------------"\
 "--------------------------------"\
 "--------------------------------"\
-"--------------------";
+"--------------------$RESET";
    cd $dir;
    for gcno in *.gcno; do
       percent=`gcov $gcno | grep "^Lines" | head -1 | cut -f2 -d ' ' | cut -f2 -d ':' | cut -f1 -d '%' | tr -d \\n`;
@@ -29,15 +46,12 @@ echo "--------------------------------"\
       covered=`awk "BEGIN {print int($ratio * $lines)}"`;
       linessum=`awk "BEGIN {print $linessum + $lines}"`;
       coveredsum=`awk "BEGIN {print $coveredsum + $covered}"`;
-      printf "%-20s %-10s %s\n" "${gcno%.gcno}.c" "$percent%" "$lines";
+      printf "$GREEN%-20s$RESET $YELLOW%-10s$RESET $CYAN%s$RESET\n" "${gcno%.gcno}.c" "$percent%" "$lines";
    done
    cd ..;
 done
 
-echo "--------------------------------"\
-"--------------------------------"\
-"--------------------------------"\
-"--------------------";
+echo "[$BLUE===================================================================================================================$RESET]";
 codecov=`awk "BEGIN {print 100 * $coveredsum / $linessum}"`
 codecov=`printf "%.2f" $codecov`;
-printf "%-20s %-10s %i\n" "Total Coverage" "$codecov%" $linessum;
+printf "$GREEN%-20s$RESET $YELLOW%-10s$RESET $CYAN%i$RESET\n" "Total Coverage" "$codecov%" $linessum;
