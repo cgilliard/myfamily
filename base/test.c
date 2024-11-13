515345055de4859202b5a5a49ac516244c842e38
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-11-13 12:33:26 -0800

    add slab allocator test

--- base/test.c
+++ base/test.c
@@ -481,3 +481,25 @@ Test(test_memmap_alloc_fail) {
 	mmap_free(ptrs, count * sizeof(Ptr));
 	memmap_cleanup(&mm2);
 }
+
+Test(test_slab_allocator) {
+	SlabAllocator sa1;
+	fam_assert(!slab_allocator_init(&sa1, 16, 100, 200));
+
+	int size = 5;
+	Ptr arr[size];
+	for (int i = 0; i < size; i++) {
+		Ptr p = slab_allocator_allocate(&sa1);
+		arr[i] = p;
+		byte *parr = slab_get(&sa1, p);
+		parr[0] = i + 2;
+	}
+
+	for (int i = 0; i < size; i++) {
+		byte *parr = slab_get(&sa1, arr[i]);
+		fam_assert_eq(parr[0], i + 2);
+		slab_allocator_free(&sa1, arr[i]);
+	}
+
+	slab_allocator_cleanup(&sa1);
+}
