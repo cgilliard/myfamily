e8222f8a89017dfe0d037b373bdca1a566bddd9a
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-11-28 18:21:51 -0800

    cleanup default

--- base/object.h
+++ base/object.h
@@ -56,16 +56,14 @@ typedef enum ObjectType {
 
 #define _(type, ...)
 
-#define PROC_DEFAULT(code)   \
-	{                        \
-		default: {           \
-			_ret__ = (code); \
-		} break;             \
-	}
+#define PROC_DEFAULT(code) \
+	default: {             \
+		_ret__ = (code);   \
+	} break;
 
 #define PROC_MATCH__(cond, ...)                                        \
 	__VA_OPT__(case cond : ({ _ret__ = (__VA_ARGS__); }); break; NONE) \
-	(PROC_DEFAULT(cond))
+	__VA_OPT__(PAREN) PROC_DEFAULT(cond) __VA_OPT__(PAREN_END)
 #define PROC_MATCH_(...) PROC_MATCH__(__VA_ARGS__)
 #define PROC_MATCH(ignore, v) PROC_MATCH_(EXPAND_ALL v)
 
