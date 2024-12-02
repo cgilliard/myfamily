0f17c98bd4f9188e3152a0da62cc09a9dae5a535
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-12-02 14:59:23 -0800

    remove files

--- base/test.h
+++ base/test.h
@@ -12,64 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <base/print_util.h>
-#include <base/util.h>
-
-#define MAX_TESTS 100
-#define MAX_TEST_NAME 1024
-
-extern int test_count;
-extern int fail_count;
-typedef void (*test_fn_ptr)(const unsigned char *);
-extern test_fn_ptr test_arr[MAX_TESTS + 1];
-extern unsigned char test_names[MAX_TESTS][MAX_TEST_NAME + 1];
-int execute_tests(unsigned char *suite_name);
-void fail_assert();
-
-#define assert(v) \
-	if (!(v)) fail_assert();
-
-#define assert_eq(v1, v2)                                                      \
-	if ((v1) != (v2)) {                                                        \
-		if (fail_count == 0)                                                   \
-			println(                                                           \
-				"--------------------------------"                             \
-				"--------------------------------"                             \
-				"--------------------------------"                             \
-				"--------------------");                                       \
-		_Generic((v1),                                                         \
-			float: print("({} != {}) ", ((float)(v1)), ((float)(v2))),         \
-			double: print("({} != {}) ", ((float)(v1)), ((float)(v2))),        \
-			unsigned char: print("({} != {}) ", (unsigned char)(v1),           \
-								 (unsigned char)(v2)),                         \
-			unsigned long long: print("({} != {}) ", (unsigned long long)(v1), \
-									  (unsigned long long)(v2)),               \
-			long long: print("({} != {}) ", (long long)(v1), (long long)(v2)), \
-			int: print("({} != {}) ", (int)(v1), (int)(v2)),                   \
-			default: ({}));                                                    \
-		fail_assert();                                                         \
+#define Suite(name) \
+	int main() {    \
+		return 0;   \
 	}
-
-#define Suite(name)                         \
-	int main() {                            \
-		int success = execute_tests(#name); \
-		if (!success) {                     \
-			return -1;                      \
-		}                                   \
-		return 0;                           \
-	}
-
-#define Test(name)                                                  \
-	void _tfwork_##name(const unsigned char *test_dir);             \
-	static void __attribute__((constructor)) __test_init_##name() { \
-		if (test_count > MAX_TESTS)                                 \
-			panic("Too many tests (MAX={})", MAX_TESTS);            \
-		int name_len = cstring_len(#name);                          \
-		if (name_len > MAX_TEST_NAME)                               \
-			panic("test name: '{}' too long!", #name);              \
-		test_arr[test_count] = &_tfwork_##name;                     \
-		copy_bytes(test_names[test_count], #name, name_len);        \
-		test_names[test_count][name_len] = 0;                       \
-		test_count++;                                               \
-	}                                                               \
-	void _tfwork_##name(const unsigned char *test_dir)
